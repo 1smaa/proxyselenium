987c307e7f88d8b36ea425f751d0f63ce6cd637d
1smaa <77068062+1smaa@users.noreply.github.com>
2021-11-12 20:25:34 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name="proxyselenium",
+      author="Ismaele Lorenzon",
+      packages=['proxyselenium'],
+      version="0.0.2",
+      author_email="lorenzonismaele@gmail.com",
+      description="A Python package to manage proxies on Selenium Chrome webdrivers.",
+      license="MIT License"
+      )
